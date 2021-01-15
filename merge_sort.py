ede7c2de2e927cd9c58f2da67ec1a97b01766449
kyleleelarson <54999183+kyleleelarson@users.noreply.github.com>
2021-01-15 14:00:20 -0500

    Add files via upload

--- merge_sort.py
+++ merge_sort.py
@@ -6,7 +6,7 @@ def merge_sort(A,p,r): # sort the array A of integers between indices p and r
 		merge_sort(A,p,q) # recursively call function on the two half-sized arrays
 		merge_sort(A,q+1,r)
 		merge(A,p,q,r) # merge the two sorted half-arrays
-	return(A)
+	return A
 		
 
 def merge(A,p,q,r): # merges sorted subarrays between indices p and q, and q+1 and r
@@ -21,7 +21,7 @@ def merge(A,p,q,r): # merges sorted subarrays between indices p and q, and q+1 a
 			B.append(A[j])
 			j+=1
 	A[p:r+1] = B
-	return(A)
+	return A
 	
 
 def main():
